cdfa40986f6e54165dff1554f0f0cea005b21e18
cottr <cottr@DESKTOP-I649USP>
2018-03-01 00:03:04 +0000

    First pass - we are creating a FFT of a sine wave... Needs to be confirmed that this is valid..

---
+++ voctune/src/main/java/com/embeddediq/voctune/analyse.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.embeddediq.voctune;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+import java.nio.ShortBuffer;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import org.jtransforms.fft.FloatFFT_1D;
+import pl.edu.icm.jlargearrays.FloatLargeArray;
+
+/**
+ *
+ * @author cottr
+ */
+public class analyse {
+    
+    // For 44,100 samples/second
+    // Nyquist-Shannon max frequency is 22,050 Hz
+
+    int sample_sz = 4096;
+    // Number of bins = 4096
+    // 22,050 / 4096 FFT bins = 5 Hz per bin
+    // This is fine for middle A4 (440 Hz)
+    // But below E3 this may not be enough resolution
+    // However, this is probably OK for Alto voice
+
+    // In time, 4096 samples equates to
+    // 4096 samples / 44100 Hz = 92 ms
+    // Which equates to 646 bpm
+    
+    // Example::
+    // If song is 72 bpm (example) and 
+    // smallest quantisation is 1/8 (quaver)
+    
+    // So long story short - we need to compromise
+    // if we want to use FFT to modifify the original
+    // audio in any way. It is fine for a "quick" visualistaion
+    // of the original audio, but not enough resolution for
+    // the actual frequency based functions.
+    
+    // We could double or quadruple sample rate 
+    // (using linear interpolation) to provide more fidelity
+    // This would give the illusion of an increased
+    // range e.g. x4 -> 1.25 Hz per bin and 1/32 notes at 72 bpm
+    
+    // Or we could increase the sample size
+    // e.g. 8192 or 16284 samples to give x4
+    // e.g. 1.25 Hz per bin but 1/2 notes at 72 bpm
+    
+    public void start() throws UnsupportedAudioFileException, IOException
+    {
+        URL x = analyse.class.getResource("/audio/audiocheck.wav");
+        // Open audio stream
+        try (AudioInputStream as = AudioSystem.getAudioInputStream(x))
+        {
+            AudioFormat af = as.getFormat();
+            long duration = as.getFrameLength();
+            
+            //int rate = (int)as.getFormat().getSampleRate(); // 44100;
+            //int bytes = as.getFormat().getFrameSize();
+            int step = af.getChannels() * af.getFrameSize();
+            int sample_sz_bytes = step * sample_sz; // Get stereo data
+
+            // Load the audio and FFT library
+            byte [] raw_data = new byte[sample_sz_bytes];
+            float [] data = new float[sample_sz];
+            //FloatLargeArray data = new FloatLargeArray(duration);
+            //data.setShort(as.read(bytes), 0);
+            FloatFFT_1D fft = new FloatFFT_1D(sample_sz);
+
+            // Process the audio
+            long offset = 0;
+            long limit = as.getFrameLength(); //  data.length();
+            while (offset < limit) {
+                as.read(raw_data, 0, sample_sz_bytes);
+                ByteBuffer bb = ByteBuffer.wrap(raw_data);
+                bb = bb.order(ByteOrder.nativeOrder());
+                ShortBuffer sb = bb.asShortBuffer();
+                // IntBuffer ib = bb.asIntBuffer();
+                for (int i=0; i<sample_sz; i++)
+                {
+                    data[i] = (float)sb.get(i);
+                }
+                fft.realForward(data);
+                offset += sample_sz;
+                
+                // TODO - draw sample
+            }
+        }
+    }
+}
---
+++ voctune/src/test/java/com/embeddediq/voctune/analyseTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.embeddediq.voctune;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author cottr
+ */
+public class analyseTest {
+    
+    public analyseTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of start method, of class analyse.
+     * @throws java.lang.Exception
+     */
+    @org.junit.Test
+    public void testStart() throws Exception {
+        System.out.println("start");
+        analyse instance = new analyse();
+        instance.start();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
